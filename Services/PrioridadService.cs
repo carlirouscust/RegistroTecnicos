7b9e9041a0b093b8ecf4f6cafd06510f24b10113
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-08-29 22:30:14 -0400

    Cree el metodo existe en el service

---
+++ Services/PrioridadService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services
+{
+    public class PrioridadService
+    {
+        private readonly Contexto _context;
+        public PrioridadService(Contexto contexto)
+        {
+            _context = contexto;
+        }
+
+        public async Task<bool> Existe(int prioridadID)
+        {
+            return await _context.prioridades
+                .AnyAsync(p => p.prioridadID == prioridadID);
+        }
+    }
+}
