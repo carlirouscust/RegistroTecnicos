6df6763bf74aebb962fbf7841f7c58677fad290f
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-08-29 21:10:12 -0400

    Cree el CS de prioridades

---
+++ Models/Prioridades.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int prioridadID { get; set; }
+        [Required(ErrorMessage = "El campo de Descripcion es obligatorio")]
+
+        public string? descripcion { get; set; }
+    }
+}
