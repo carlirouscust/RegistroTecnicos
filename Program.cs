2bf0568a4eba96157ab1fa8e5f6581c449ec68ae
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-08-30 18:47:33 -0400

    inyecte el service

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Components;
 using RegistroTecnicos.DAL;
+using RegistroTecnicos.Services;
 
 namespace RegistroTecnicos
 {
@@ -19,6 +20,8 @@ namespace RegistroTecnicos
 
             //agregamos el contexto al builder con el ConStr
             builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
+
+            builder.Services.AddScoped<PrioridadService>();
 
             var app = builder.Build();
 
