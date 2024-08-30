48b432983f4e037ee21ba8e352cd70063676d9af
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-08-30 18:43:42 -0400

    cree los metodos existe, insertar, modificar, guardar, eliminar, buscar y listar

--- Services/PrioridadService.cs
+++ Services/PrioridadService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.DAL;
 using RegistroTecnicos.Models;
+using System.Diagnostics.Contracts;
 using System.Linq.Expressions;
 
 namespace RegistroTecnicos.Services
@@ -18,5 +19,48 @@ namespace RegistroTecnicos.Services
             return await _context.prioridades
                 .AnyAsync(p => p.prioridadID == prioridadID);
         }
+
+        public async Task<bool> Insertar(Prioridades prioridades)
+        {
+            _context.prioridades .Add(prioridades);
+            return await _context.SaveChangesAsync() > 0; 
+        }
+
+        public async Task<bool> Modificar(Prioridades prioridades)
+        {
+            _context.Update(prioridades);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Guardar(Prioridades prioridad)
+        {
+            if (!await Existe(prioridad.prioridadID))
+                return await Insertar(prioridad);
+            else
+                return await Modificar(prioridad);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            var prioridades = await _context.prioridades.
+                Where(P => P.prioridadID == id).ExecuteDeleteAsync();
+            return prioridades > 0;
+        }
+
+        public async Task<Prioridades?> Buscar(int id)
+        {
+            return await _context.prioridades.
+                AsNoTracking()
+                .FirstOrDefaultAsync(P => P.prioridadID == id);
+        }
+
+        public List<Prioridades> Listar(Expression<Func<Prioridades, bool>> criterio)
+        {
+            return _context.prioridades.
+                AsNoTracking()
+                .Where(criterio)
+                .ToList();
+        }
+
     }
 }
