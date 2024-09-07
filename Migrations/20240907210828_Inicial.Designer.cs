ff4bcfd1c48f16119da498e552f7eada2eebd12f
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-09-07 17:41:45 -0400

    Db update

--- Migrations/20240907210828_Inicial.Designer.cs
+++ Migrations/20240907210828_Inicial.Designer.cs
@@ -11,7 +11,7 @@ using RegistroTecnicos.DAL;
 namespace RegistroTecnicos.Migrations
 {
     [DbContext(typeof(Contexto))]
-    [Migration("20240907174754_Inicial")]
+    [Migration("20240907210828_Inicial")]
     partial class Inicial
     {
         /// <inheritdoc />
