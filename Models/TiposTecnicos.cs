8b1fbbca2eeff591a3ba04a47515469590a5cb3e
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-09-06 21:45:44 -0400

    Se creo el TiposTecnicos

---
+++ Models/TiposTecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class TiposTecnicos
+{
+    [Key]
+    public int TiposTecnicosID { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+
+    public string? Descripcion { get; set; }
+    
+}
