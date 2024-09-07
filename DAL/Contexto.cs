65a5650934a8d42a8bd3e58506c5b7207239205d
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-09-07 15:06:15 -0400

    Se agrego un override para que varios tecnicos puedan tener un tipo de tecnico

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -12,4 +12,15 @@ public class Contexto : DbContext
     public DbSet<Tecnicos> Tecnicos { get; set; }
 
     public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Tecnicos>()
+            .HasOne(tt => tt.tiposTecnicos)
+            .WithMany(t => t.Tecnicos)
+            .HasForeignKey(t => t.TiposTecnicosID);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
