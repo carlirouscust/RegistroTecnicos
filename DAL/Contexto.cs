7cde40d5a5edc2b6c48b3a6b4afc2d2d69e7313f
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-09-11 03:50:13 -0400

    Se creo la propiedad de Dbset de Trabajos

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -16,6 +16,8 @@ public class Contexto : DbContext
 
     public DbSet<Clientes> Clientes { get; set; }
 
+    public DbSet<Trabajos> Trabajos { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
 
