4e1819a6c9206063092daabf44d4c60c3f1ad02b
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-09-18 01:41:00 -0400

    Se creo el modelo prioridades

---
+++ Models/Prioridades.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace RegistroTecnicos.Models;
+
+public class Prioridades
+{
+    [Key]
+    public int PriodidadesID { get; set; }
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public int? Tiempo { get; set; }
+}
