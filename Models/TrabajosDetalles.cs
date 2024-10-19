8dcd4b7c35804bd11061964f7bb83e8ed1526167
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-10-18 22:54:06 -0400

    se creo el modelo trabajos detalles

---
+++ Models/TrabajosDetalles.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class TrabajosDetalles
+{
+    [Key]
+    public int detalleId { get; set; }
+
+    public Trabajos? Trabajos { get; set; }
+
+    [ForeignKey("trabajoId")]
+   
+    public int trabajoId { get; set; }
+    
+    public Articulos? Articulos { get; set; }
+
+    [ForeignKey("articuloId")]
+    public int articuloId { get; set; }
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public int? cantidad { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public decimal? precio { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public decimal? costo { get; set; }
+}
