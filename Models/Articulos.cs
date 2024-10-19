c00f4f1728b88b5fb74f151199cfe69595a91b45
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-10-18 22:53:32 -0400

    se creo el modelo de articulos

---
+++ Models/Articulos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Articulos
+{
+    [Key]
+    public int articuloId { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public string? descripcion { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public decimal? costo { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public decimal? precio { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public decimal? existencia { get; set; }
+}
