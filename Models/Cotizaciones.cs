eedd6f5bb0beea566517c6a9282dfc4faa8138a7
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-10-25 21:27:30 -0400

    se creo el modelo cotizaciones

---
+++ Models/Cotizaciones.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Cotizaciones
+{
+    [Key]
+    public int cotizacionesId { get; set; }
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    public Clientes? clientes { get; set; }
+
+    [ForeignKey("clientes")]
+    public int ClientesID { get; set; }
+
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public string? observacion { get; set; }
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public int? monto { get; set; }
+
+    public ICollection<CotizacionesDetalles> CotizacionesDetalles { get; set; }
+}
