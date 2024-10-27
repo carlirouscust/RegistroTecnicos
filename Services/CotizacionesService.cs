2a49d5d01421f6a98e1fe1df71971b5249f78b21
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-10-26 20:46:25 -0400

    se cambio el contexto a Dbfactory

--- Services/CotizacionesService.cs
+++ Services/CotizacionesService.cs
@@ -5,28 +5,25 @@ using System.Linq.Expressions;
 
 namespace RegistroTecnicos.Services;
 
-public class CotizacionesService
+public class CotizacionesService(IDbContextFactory<Contexto> DbFactory)
 {
-    private readonly Contexto _context;
-    public CotizacionesService(Contexto contexto)
-    {
-        _context = contexto;
-    }
-
     public async Task<bool> Existe(int cotizacionesId)
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         return await _context.Cotizaciones
             .AnyAsync(T => T.cotizacionesId == cotizacionesId);
     }
 
     public async Task<bool> Insertar(Cotizaciones cotizaciones)
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         _context.Cotizaciones.Add(cotizaciones);
         return await _context.SaveChangesAsync() > 0;
     }
 
     public async Task<bool> Modificar(Cotizaciones cotizaciones)
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         _context.Cotizaciones.Update(cotizaciones);
         return await _context.SaveChangesAsync() > 0;
     }
@@ -41,6 +38,7 @@ public class CotizacionesService
 
     public async Task<bool> Eliminar(int id)
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         var trabajos = await _context.Cotizaciones.
             Where(T => T.cotizacionesId == id).ExecuteDeleteAsync();
         return trabajos > 0;
@@ -48,6 +46,7 @@ public class CotizacionesService
 
     public async Task<Cotizaciones?> Buscar(int id)
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         return await _context.Cotizaciones
             .AsNoTracking()
             .Include(c => c.clientes)
@@ -56,8 +55,9 @@ public class CotizacionesService
             .FirstOrDefaultAsync(c => c.cotizacionesId == id);
     }
 
-    public List<Cotizaciones> Listar(Expression<Func<Cotizaciones, bool>> criterio)
+    public async Task <List<Cotizaciones>> Listar(Expression<Func<Cotizaciones, bool>> criterio)
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         return _context.Cotizaciones
             .AsNoTracking()
             .Include(c => c.clientes)
@@ -68,6 +68,7 @@ public class CotizacionesService
 
     public async Task<List<CotizacionesDetalles>> ObtenerDetallesCotizacionId(int cotizacionId)
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         var detalles = await _context.CotizacionesDetalles
             .Where(c => c.cotizaciones.cotizacionesId == cotizacionId)
             .ToListAsync();
@@ -77,21 +78,25 @@ public class CotizacionesService
 
     public async Task<List<Clientes>> ObtenerClientes()
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         return await _context.Clientes.ToListAsync();
     }
 
     public async Task<List<Cotizaciones>> ObtenerCotizaciones()
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         return await _context.Cotizaciones.ToListAsync();
     }
 
     public async Task<List<CotizacionesDetalles>> ObtenerDetalle()
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         return await _context.CotizacionesDetalles.ToListAsync();
 
     }
     public async Task<List<Articulos>> ObtenerArticulos()
     {
+        await using var _context = await DbFactory.CreateDbContextAsync();
         return await _context.Articulos.ToListAsync();
     }
 }
